46bcdbb574c89cb5ada8d0555da511f36505a704
Jennifer Roques <jenniferroques@gmail.com>
2022-01-01 18:38:11 -0600

    commit tree 45478ff460c5d0181e3497558b77def5c35f270f

--- index.js
+++ index.js
@@ -1,1 +1,69 @@
-// Your code here
+
+const createEmployeeRecord = (recArray) => {
+    return {
+        firstName: recArray[0],
+        familyName: recArray[1],
+        title: recArray[2],
+        payPerHour: recArray[3],
+        timeInEvents: [],
+        timeOutEvents: [],
+    }
+}
+
+const createEmployeeRecords = (recordsArray) => {
+    return recordsArray.map(rec => createEmployeeRecord(rec))
+}
+
+
+let createTimeInEvent = function (datestamp) {
+    let [date, hour] = datestamp.split(" ");
+
+    this.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(hour, 10),
+        date: date
+    })
+    return this
+}
+
+let createTimeOutEvent = function (datestamp) {
+    let [date, hour] = datestamp.split(" ");
+
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(hour, 10),
+        date: date
+    })
+    return this
+}
+
+const hoursWorkedOnDate = function (targetDate) {
+    const inEvent = this.timeInEvents.find(inEvent => inEvent.date === targetDate)
+    const outEvent = this.timeOutEvents.find(outEvent => outEvent.date === targetDate)
+    return (outEvent.hour - inEvent.hour) / 100
+}
+
+const wagesEarnedOnDate = function (targetDate) {
+    return hoursWorkedOnDate.call(this, targetDate) * this.payPerHour
+}
+
+
+/*const hoursWorkedOnDate = function (targetDate) {
+    const iEvent = this.timeInEvents.find(iEvent => iEvent.date === targetDate)
+    const oEvent = this.timeOutEvents.find(oEvent => oEvent.date === targetDate)
+
+    return (oEvent.hour - iEvent.hour) / 100
+}*/
+
+
+const allWagesFor = function () {
+    const eligibleDates = this.timeInEvents.map(function (e) {
+        return e.date
+    })
+
+    const payable = eligibleDates.reduce(function (memo, d) {
+        return memo + wagesEarnedOnDate.call(this, d)
+    }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
+
+    return payable
+}
